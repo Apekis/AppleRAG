86277fd0cd690f387c2bb0a7b9df4ee0737667b9
Andreas Koutsopoulos <andreas.koutsopoulos.xa@renesas.com>
2026-01-07 14:16:43 +0100

    adjust app.py

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://applepayrag.vercel.app"
+        "https://apple-rag.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
