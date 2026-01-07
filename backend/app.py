014d5cda2ec59897e7d536bf16984fcf5f6cae02
Andreas Koutsopoulos <andreas.koutsopoulos.xa@renesas.com>
2026-01-07 13:38:10 +0100

    adjust starlette import

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ import os
 import sys
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
-from sse_starlette.sse import EventSourceResponse
+from starlette.responses import EventSourceResponse
 from dotenv import load_dotenv
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
